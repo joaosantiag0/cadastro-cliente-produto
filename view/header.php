ffd02efb0ce168836765c5e8fc5958e8d7ad0d74
jonhbala <joao_santiago@outlook.com>
2014-09-21 20:22:25 -0300

    Commit files of layout

---
+++ view/header.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Cadastro</title>
+
+    <!-- Core CSS - Include with every page -->
+    <link href="view/css/bootstrap.min.css" rel="stylesheet">
+    <link href="view/font-awesome/css/font-awesome.css" rel="stylesheet">
+
+    <!-- SB Admin CSS - Include with every page -->
+    <link href="view/css/sb-admin.css" rel="stylesheet">
+
+
+        <!-- Core Scripts - Include with every page -->
+    <script src="view/js/jquery-1.10.2.js"></script>
+    <script src="view/js/bootstrap.min.js"></script>
+    <script src="view/js/plugins/metisMenu/jquery.metisMenu.js"></script>
+
+    <!-- Page-Level Plugin Scripts - Dashboard -->
+    <script src="view/js/plugins/morris/raphael-2.1.0.min.js"></script>
+    <script src="view/js/plugins/morris/morris.js"></script>
+
+    <!-- SB Admin Scripts - Include with every page -->
+    <script src="view/js/sb-admin.js"></script>
+
+    <!-- Page-Level Demo Scripts - Dashboard - Use for reference -->
+    <script src="view/js/demo/dashboard-demo.js"></script>
+
+    <script type="text/javascript">
+    $(".modal").on("click", function(){
+    	$(".btn").button();
+    });
+    </script>
+
+</head>
+
+<body>
+
+   <div id="wrapper">
+
+        <nav class="navbar navbar-default navbar-fixed-top" role="navigation" style="margin-bottom: 0">
+            <div class="navbar-header">
+                <a class="navbar-brand" href="index.php">Cadastro</a>
+            </div>
+
+
+
+                    <div class="navbar-default navbar-static-side" role="navigation">
+                <div class="sidebar-collapse">
+                    <ul class="nav" id="side-menu">
+                       
+                        <li>
+                            <a href="index.php"><i class="fa fa-home fa-fw"></i> Principal</a>
+                        </li>
+                        <li>
+                            <a href="#"><i class="fa fa-male fa-fw"></i> Adicionar Usuário</a>
+                            
+                        </li>
+                        <li>
+                            <a href="#"><i class="fa fa-group fa-fw"></i> Listar Usuários</a>
+
+                        </li>
+                        <li>
+                        	<a href="#"><i class="fa fa-plus-square fa-fw"></i> Adicionar Produto </a>
+                        </li>
+                        <li>
+                        	<a href="#"><i class="fa fa-shopping-cart fa-fw"></i> Listar Produtoss </a>
+                        </li>
+                    </ul>
+                    <!-- /#side-menu -->
+                </div>
+                <!-- /.sidebar-collapse -->
+            </div>
+            <!-- /.navbar-static-side -->
+        </nav>
+     
